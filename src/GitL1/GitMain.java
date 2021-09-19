7303cb390a103cd4306eec1f0b02269b52257aad
antonazzot <anton.azzot@yandex.ru>
2021-09-19 09:46:38 +0300

    Waiting name method add

--- src/GitL1/GitMain.java
+++ src/GitL1/GitMain.java
@@ -1,10 +1,20 @@
 package GitL1;
+
+import java.util.Scanner;
+
 /*
 Goals:
 It's need some public static method, that wait entering user name.
  */
 public class GitMain {
     public static void main(String[] args) {
+        System.out.println("Hello " + waitMethod() + "!");
         
+    }
+    public static String waitMethod () {
+        Scanner sc =null;
+        System.out.println("Enter your name");
+        sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 }
